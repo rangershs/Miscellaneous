cbf51850445e9fb5838b6738cf5bc805f2a5a1fc
ssh <rangershs@163.dom>
2018-07-25 10:45:08 +0800

    update FAQ.txt and operator_overload.cpp,add some info

--- operator_overload.cpp
+++ operator_overload.cpp
@@ -6,6 +6,7 @@
 	3	[<<],[>>]必须重载为全局函数，若重载为成员函数，则需要去类istream和ostream中重载，这是不可能的
 	4	以类的成员函数重载时，一元运算符没有参数，二元运算符有一个参数
 		以全局函数重载时，一元运算符有一个参数，二元运算符有两个参数，且至少一个参数是类对象
+	5	[++],[--]分前置和后置两种情况，允许以成员函数和友元函数两种方式重载，其中形参int的作用是区分前置和后置，C++选择增加一个占位参数实现后置版本
 */
 
 #include <iostream>
@@ -15,11 +16,17 @@ class Complex
 public:
 	Complex(double _real=1.1,double _image=2.2):real(_real),image(_image) {}
 	~Complex() {}
+
 	Complex operator+(const Complex& A) const;
 	friend Complex operator-(const Complex& A,const Complex& B);
 	Complex& operator+=(const Complex& A);
 	friend std::istream& operator>>(std::istream& is,Complex& A);
 	friend std::ostream& operator<<(std::ostream& os,const Complex& A);
+
+	Complex& operator++();
+	Complex operator++(int);
+	friend Complex& operator--(Complex& A);
+	friend Complex operator--(Complex& A,int);
 private:
 	double real;
 	double image;
@@ -58,6 +65,36 @@ std::ostream& operator<<(std::ostream& os,const Complex& A)
 {
 	os << A.real << A.image;
 	return os;
+}
+
+Complex& Complex::operator++()
+{
+	this->real++;
+	this->image++;
+	return *this;
+}
+
+Complex Complex::operator++(int)
+{
+	Complex tmp(*this);
+	this->real++;
+	this->image++;
+	return tmp;
+}
+
+Complex& operator--(Complex& A)
+{
+	A.real--;
+	A.image--;
+	return A;
+}
+
+Complex operator--(Complex& A,int)
+{
+	Complex tmp(A);
+	A.real--;
+	A.image--;
+	return tmp;
 }
 
 int main()
