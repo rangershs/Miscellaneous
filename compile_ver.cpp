509ce0dfc885f1589022c39853c20431d9ed6307
shs <shs@shsde-MacBook-Air.local>
2019-01-06 22:40:47 +0800

    optimizing it

--- compile_ver.cpp
+++ compile_ver.cpp
@@ -1,37 +1,30 @@
 #include <iostream>
-#include <cstring>
-#include <cstdio>
-#include <cstdlib>
+#include <string>
 
-//	由调用者释放申请的内存
-const char* GetCompileVersion()
+std::string GetCompileVersion()
 {
-	const char* Months[] = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
-	const char* date = __DATE__;
-	const char* time = __TIME__;
-	char mon[4]; int year; int day;
-	sscanf(date, "%s %d %d", mon, &day, &year);
-	unsigned int i_mon = 0;
-	for (; i_mon < sizeof(Months) / sizeof(char*); ++i_mon)
-	{
-		if (strcmp(Months[i_mon], mon) == 0)
-			break;
-	}
-	int hour; int min; int sec;
-	sscanf(time, "%d:%d:%d", &hour, &min, &sec);
-	char* ver = new char[32];
-	sprintf(ver, "%04d%02d%02d_%02d%02d%02d", year, i_mon + 1, day, hour, min, sec);
-	return ver;
+    const char* Months[] = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+    const char* date = __DATE__;
+    const char* time = __TIME__;
+    char mon[4]; int year; int day;
+    sscanf(date, "%s %d %d", mon, &day, &year);
+    unsigned int i_mon = 0;
+    for (; i_mon < sizeof(Months) / sizeof(char*); ++i_mon)
+    {
+        if (strcmp(Months[i_mon], mon) == 0)
+            break;
+    }
+    int hour; int min; int sec;
+    sscanf(time, "%d:%d:%d", &hour, &min, &sec);
+    char ver[32];
+    snprintf(ver, 32, "%04d%02d%02d-%02d%02d%02d", year, i_mon + 1, day, hour, min, sec);
+    return ver;
 }
 
-int main(int argc,char** argv)
+int main()
 {
-	const char* ver = GetCompileVersion();
-	std::cout << ver << std::endl;
-	if(ver != NULL)
-	{
-		delete[] ver;
-		ver = NULL;
-	}
-	return 0;
+    std::string ver = GetCompileVersion();
+    std::cout << ver << std::endl;
+
+    return 0;
 }
